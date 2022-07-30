6cc79b317de6dfef25cb91943cc065b921284862
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2022-07-29 23:02:59 -0500

    Create WebCleanText.py
     - When we make web scraping the text can contain extra-spaces, new line character, and so on. This small function delete that.
     - Consume string and return string
     - No dependecies

---
+++ data munging/WebCleanText.py
@@ -0,0 +1,16 @@
+# A very simple step: When we make web scraping the text can contain extra-spaces, new line character and so on. This function delete that.
+# Consume string and return string
+# No dependecies
+def WebCleanText(a):
+	B=''
+  A=' '
+  a.replace(',',A).replace("'",B).replace('  ',A).replace('\t',B).replace('\r',B).replace('\n',B).replace('[',A).replace(']',A)
+  a.split(A)
+  C=len(a)-1
+  D=0
+	if a[0]==A:D=1
+	else:D=0
+	if a[C]==A:E=C-1
+	else:E=C
+	F=str(a[D:E])
+  return F
