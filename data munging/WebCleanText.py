a25a7ff45a1b99df1c6900d346b8d098237db02b
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2022-07-29 23:17:07 -0500

    Update WebCleanText.py
    tabs 0_o

--- data munging/WebCleanText.py
+++ data munging/WebCleanText.py
@@ -3,14 +3,14 @@
 # No dependecies
 def WebCleanText(a):
 	B=''
-  A=' '
-  a.replace(',',A).replace("'",B).replace('  ',A).replace('\t',B).replace('\r',B).replace('\n',B).replace('[',A).replace(']',A)
-  a.split(A)
-  C=len(a)-1
-  D=0
+	A=' '
+	a.replace(',',A).replace("'",B).replace('  ',A).replace('\t',B).replace('\r',B).replace('\n',B).replace('[',A).replace(']',A)
+	a.split(A)
+	C=len(a)-1
+	D=0
 	if a[0]==A:D=1
 	else:D=0
 	if a[C]==A:E=C-1
 	else:E=C
 	F=str(a[D:E])
-  return F
+	return F
