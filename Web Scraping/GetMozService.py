dbb94cbda51809711f3596dd31fdcc8ee3839539
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2022-07-31 21:49:01 -0500

    Exceptions

--- Web Scraping/GetMozService.py
+++ Web Scraping/GetMozService.py
@@ -1,11 +1,16 @@
 # Start a web driver service with mozilla
 # None to ingest, return a web driver service with mozilla
 # Selenium and webdriver as dependencies
+# Exceptions
+class NonInstall(Exception):
+	def __init__(NonInstall,error,message="o_0 \n Driver not present, and can't install"):NonInstall.error=error;NonInstall.message=message;super().__init__(NonInstall.message)
+	def __str__(NonInstall):return f"{NonInstall.error} -> {NonInstall.message}"
+#
 def GetMozService():
-    # instalamos el Gecko Driver solo la primera vez es necesario
+    # instalamos el Gecko Driver solo la primera vez es necessario
     from selenium import webdriver
     from selenium.webdriver.firefox.options import Options
-    from selenium.webdriver.firefox.service import Service as FirefoxService
+    from selenium.webdriver.firefox.service import Service
     from webdriver_manager.firefox import GeckoDriverManager
     options = Options()
     options.add_argument( '--disable-blink-features=AutomationControlled' )
@@ -18,7 +23,6 @@ def GetMozService():
             driver = webdriver.Firefox(options=options)            
         except:
             print('trantando de instalar webdriver')
-            driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), executable_path='geckodriver.exe')
-    except: print('0_o error de navegacion')
+            driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), executable_path='geckodriver.exe')
+    except: raise NonInstall(Exception)
     return driver
-#
